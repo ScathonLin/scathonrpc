a2c89670ede1ddb28ed3e67ede9c6ab4a8cc064b
ScathonLin <ScathonLin@126.com>
2019-05-08 00:25:40 +0800

    protobuf整合Protostuff；通信打通

---
+++ scathonrpc_common/src/test/java/com/scathon/tech/rpc/common/proto/ProtobufTest.java
@@ -0,0 +1,76 @@
+package com.scathon.tech.rpc.common.proto;
+
+import com.google.protobuf.Any;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.scathon.tech.rpc.common.entity.RequestMessage;
+import com.scathon.tech.rpc.common.utils.ProtostuffCodecUtils;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * TODO Function The Class Is.
+ *
+ * @ClassName ProtobufTest.
+ * @Description TODO.
+ * @Author linhd eng:ScathonLin
+ * @Date 2019/5/7
+ * @Version 1.0
+ */
+public class ProtobufTest {
+    @Test
+    public void anyTest() throws InvalidProtocolBufferException {
+        RequestMessage reqMsg =
+                new RequestMessage().setServiceName("hello").setRequestUUID("ajsdkfj").setParameters(new Object[]{1,
+                        "linhd"});
+        byte[] bytes1 = ProtostuffCodecUtils.serialize(reqMsg, RequestMessage.class).get();
+        System.out.println(bytes1.length);
+        RequestMessage deserialize = ProtostuffCodecUtils.deserialize(bytes1, RequestMessage.class).get();
+        System.out.println(deserialize.toString());
+
+
+        RequestMsgEntity.RequestMessage.Builder builder = RequestMsgEntity.RequestMessage.newBuilder();
+
+        ParamObj paramObj = new ParamObj();
+        paramObj._1 = 1;
+        paramObj._2 = "linhd";
+
+        User user = new User();
+        user.id = 222;
+        user.name = "scathon";
+        paramObj._3 = user;
+
+        List<Object> objs = new LinkedList<>();
+        objs.add(int.class);
+        objs.add(String.class);
+        objs.add(Object.class);
+
+        paramObj.objs = objs;
+
+        byte[] bytes = ProtostuffCodecUtils.serialize(paramObj, ParamObj.class).get();
+        builder.setParamObjs(Any.newBuilder().setValue(ByteString.copyFrom(bytes)).build());
+        builder.setRequestUUID("111uuuid");
+        builder.setMethodName("findUserById");
+        builder.setServiceName("userService");
+        byte[] serialBytes = builder.build().toByteArray();
+        System.out.println(serialBytes.length);
+        RequestMsgEntity.RequestMessage.Builder newBuilder = builder.mergeFrom(serialBytes);
+        System.out.println(newBuilder.getServiceName());
+        System.out.println(newBuilder.getRequestUUID());
+        System.out.println(newBuilder.getMethodName());
+        System.out.println(newBuilder.getParamObjs());
+
+        Optional<ParamObj> obj =
+                ProtostuffCodecUtils.deserialize(newBuilder.getParamObjs().getValue().toByteArray(), ParamObj.class);
+        System.out.println(obj.get());
+        RequestMsgEntity.RequestMessage msg = newBuilder.build();
+        ParamObj paramObj1 = ProtostuffCodecUtils.deserialize(msg.getParamObjs().getValue().toByteArray(),
+                ParamObj.class).get();
+        System.out.println(paramObj1);
+
+
+    }
+}
