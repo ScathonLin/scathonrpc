4ff75b93970f7dd0987aaa49c63eb72a1fb06590
ScathonLin <ScathonLin@126.com>
2019-05-19 23:48:59 +0800

    TPS测试

--- scathonrpc_client_simple/src/test/java/com/scathon/tech/rpc/client/simple/service/RpcPerformanceTest.java
+++ scathonrpc_client_simple/src/test/java/com/scathon/tech/rpc/client/simple/service/RpcPerformanceTest.java
@@ -3,14 +3,16 @@ package com.scathon.tech.rpc.client.simple.service;
 import com.scathon.tech.rpc.client.initializer.RpcClientAutoInitConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
- * TODO Function The Class Is.
+ * 性能测试.
  *
- * @ClassName BeanRegistryTest.
+ * @ClassName RpcPerformanceTest.
  * @Description TODO.
  * @Author linhd eng:ScathonLin
  * @Date 2019/5/3
@@ -18,17 +20,22 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = RpcClientAutoInitConfig.class)
-public class BeanRegistryTest {
+public class RpcPerformanceTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RpcPerformanceTest.class);
     @Autowired
     private UserServiceClient userServiceClient;
 
     @Test
     public void testCustomRegistry() {
+        int loopCount = 1000;
         System.out.println(userServiceClient != null);
-        for (int i = 0; i < 1000; i++) {
-            System.out.println(i + "=====================");
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < loopCount; i++) {
             System.out.println(userServiceClient.findUserById(11111));
-            System.out.println(userServiceClient.findOrderByUserInfo());
+            //System.out.println(userServiceClient.findOrderByUserInfo());
         }
+        long cost = System.currentTimeMillis() - start;
+        System.out.println("=====>COST: " + cost + "ms");
+        System.out.println("tps: " + (loopCount * 1.0 / (cost * 1.0 / 1000)));
     }
 }
